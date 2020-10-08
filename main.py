b244be233f55125fa251107b22f68524b6b81369
Evan-Beaudoin <71400127+Evan-Beaudoin@users.noreply.github.com>
2020-10-08 14:12:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 # Created by: Evan
 # Created on: Oct.2020
-# This program reads the value of a potentiometer
+# This program controls a servo with a potentiomketer
 
 from microbit import *
 
 
 
 pin2.set_analog_period(10)
+display.show(Image.HEART)
 
 while True:
     potentiometer = pin1.read_analog()
-    
-    pin2.write_analog(potentiometer)
-    sleep(200)
+    potentiometer = potentiometer/5.683333
+    if potentiometer < 40:
+        potentiometer = 40
+    pin2.write_analog(int(potentiometer))
+    print(int(potentiometer))
+    sleep(500)
 
 
